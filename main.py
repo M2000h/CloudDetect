b5324fb8ab53000a5e0855e2aaaae8c6deff3067
m2000h <mashakura@edu.hse.ru>
2020-02-16 21:10:18 +0300

    remooving clouds after bluring

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import cv2
 import tifffile as tiff
-a = tiff.imread('example.tiff')
-a[a > 150] = 0
+import numpy as np
+filename = 'example.tiff'
+a = tiff.imread(filename)
+c = a.copy()
+# b = np.ndarray(shape=(len(a), len(a[0]),3), dtype=int)
+# a = a[((x[0] for y in a for x in y)=0)]
+a = cv2.blur(a, (50,50))
+b = np.all(a < (200, 200, 200), axis=-1)
+c[b==0]=0
+
 tiff.imsave('new.tiff', a)
+tiff.imsave('new1.tiff', c)
+tiff.imsave("output"+filename, c)
